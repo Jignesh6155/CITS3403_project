c7e3adb3a47649d4399c414b26a80d66336fbd1c
Checkmate1941 <adriaan.vanderberg@outlook.com>
2025-05-16 21:45:01 +0800

    Updated run with  new config

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from app import create_app
 from app.models import db
 from app.config import DevelopmentConfig
+from app.config import ProductionConfig
+from app.config import TestingConfig
 
+# Change this to the appropriate config for the environment you're running in
+# Choose from DevelopmentConfig, ProductionConfig, or TestingConfig
 app = create_app(DevelopmentConfig)
 
 with app.app_context():
